435e7817ee396f7bb3ff4532cfd76edf61f10ca6
Samuel Jones <sam@sjoness.co.uk>
2016-04-17 17:34:10 +0100

    Implement a scoreboard activity to show last three high scores

--- app/src/main/java/com/example/p12202749/tapcopter/activities/MainActivity.java
+++ app/src/main/java/com/example/p12202749/tapcopter/activities/MainActivity.java
@@ -19,12 +19,21 @@ public class MainActivity extends AppCompatActivity {
         // which will allow the application to navigate to the game view when the user presses the
         // button.
         Button playButton = (Button) findViewById(R.id.play_button);
+        Button scoreboardButton = (Button) findViewById(R.id.scoreboard_button);
         Button settingsButton = (Button) findViewById(R.id.settings_button);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        scoreboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScoreboardActivity.class);
                 startActivity(intent);
             }
         });
